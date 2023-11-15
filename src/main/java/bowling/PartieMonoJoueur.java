71c0cba8524a203e54f8b6e1cfa41a9d8985a5c1
lisacapitaine <147838571+lisacapitaine@users.noreply.github.com>
2023-11-15 15:41:17 +0100

    Update PartieMonoJoueur.java

--- src/main/java/bowling/PartieMonoJoueur.java
+++ src/main/java/bowling/PartieMonoJoueur.java
@@ -6,11 +6,28 @@ package bowling;
  * final de ce joueur
  */
 public class PartieMonoJoueur {
+private int quilles ; 
+private int score ; 
+private Tour [] tour ; 
+private String joueur ; 
+private int lancer ; 
+private int nbTour ; 
+private int nbLancer ; 
 
 	/**
 	 * Constructeur
 	 */
-	public PartieMonoJoueur() {
+	public PartieMonoJoueur(int score,String joueur, int lancer) {
+	this.quilles = 10 ; 
+	this.score = score ; 
+	this.joueur = joueur;
+	this.tour = new Tour[11]; 
+	for(var i= 0; i < 11 ; i++) {
+	tour[i]=0 ; 
+	}
+	this.lancer = lancer ; 
+	this.nbTour = 0 ; 
+	this.nbLancer = 0 ; 
 	}
 
 	/**
@@ -21,9 +38,22 @@ public class PartieMonoJoueur {
 	 * @return vrai si le joueur doit lancer à nouveau pour continuer son tour, faux sinon	
 	 */
 	public boolean enregistreLancer(int nombreDeQuillesAbattues) {
-		throw new UnsupportedOperationException("Pas encore implémenté");
+		nbLancer = nbLancer + 1 ; 
+		if ( quilles - nombreDeQuillesAbattues ==0 || nbLancer== 2 ){
+			quilles = 10 ; 
+			return false ; 
+			
+		}
+		if (nbLancer == 2){
+			nbLancer =0 ; 
+		}
+		
+		else quilles = quilles - nombreDeQuillesAbattues ; 
+		return true; 
+		
 	}
 
+	
 	/**
 	 * Cette méthode donne le score du joueur.
 	 * Si la partie n'est pas terminée, on considère que les lancers restants
@@ -31,22 +61,27 @@ public class PartieMonoJoueur {
 	 * @return Le score du joueur
 	 */
 	public int score() {
-		throw new UnsupportedOperationException("Pas encore implémenté");
+		for ()
 	}
 
 	/**
 	 * @return vrai si la partie est terminée pour ce joueur, faux sinon
 	 */
 	public boolean estTerminee() {
-		throw new UnsupportedOperationException("Pas encore implémenté");
-	}
+		lancer = lancer-1 ; 
+		if (lancer == 0)	{
+			return true ; 
+
+		}
+		else return false ; 
+		}
 
 
 	/**
 	 * @return Le numéro du tour courant [1..10], ou 0 si le jeu est fini
 	 */
 	public int numeroTourCourant() {
-		throw new UnsupportedOperationException("Pas encore implémenté");
+	
 	}
 
 	/**
