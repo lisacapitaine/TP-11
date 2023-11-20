cafd919cbd4f49f239bf6370d1417a59948481db
lisacapitaine <147838571+lisacapitaine@users.noreply.github.com>
2023-11-20 11:04:15 +0100

    Create Lancer.java

---
+++ src/main/java/bowling/Lancer.java
@@ -0,0 +1,14 @@
+package bowling;
+
+public class Lancer {
+	private int nbQuille;
+
+	public Lancer(int nbQuille) {
+		this.nbQuille = nbQuille;
+	}
+
+	public int getNbQuille() {
+		return nbQuille;
+	}
+}
+
