bc7a3892ae3b1347bebe928509b088e431992068
lisacapitaine <147838571+lisacapitaine@users.noreply.github.com>
2023-11-20 11:03:28 +0100

    Update Tour.java

--- src/main/java/bowling/Tour.java
+++ src/main/java/bowling/Tour.java
@@ -1,59 +1,75 @@
+package bowling;
+
+import java.util.ArrayList;
+
 public class Tour {
+	private int tour;
+	private ArrayList<Lancer> lesLancers = new ArrayList<>();
+	private int coup = 1;
+	private boolean termine = false;
 
-private int lance1 ; 
-private int lance2 ; 
-private int compteur; 
-private static int add =0; 
+	public Tour(int tour) {
+		this.tour = tour;
+	}
 
+	public boolean estUnStrike() {
+		return lesLancers.size() == 1 && lesLancers.get(0).getNbQuille() == PartieMonoJoueur.nbDeQuilles;
+	}
 
-public Tour (int lance1 , int lance2){
-    this.lance1 = lance1 ; 
-    this.lance2 = lance2 ; 
-    compteur = add+1 ; 
-    add= add+1 ; 
+	public boolean estUnSpare() {
+		return coup == 2 && getScore(1) + getScore(2) == PartieMonoJoueur.nbDeQuilles && tour != PartieMonoJoueur.nbDeTours;
+	}
+
+	
+	public int getCoupSuivant() {
+		return coup;
+	}
+	
+	public int getScore(int numLancer) {
+		if (numLancer >= 1 && numLancer <= lesLancers.size()) {
+			return lesLancers.get(numLancer - 1).getNbQuille();
+		} else {
+			return 0;
+		}
+	}
+
+	public int getScore() {
+		int score = 0;
+		for (Lancer lancer : lesLancers) {
+			score += lancer.getNbQuille();
+		}
+		return score;
+	}
+
+	
+	public boolean estTermine() {
+		return termine;
+	}
+	
+
+	public boolean enregistreLancer(Lancer lancer) {
+		if (coup == 1) {
+			lesLancers.add(lancer);
+			if (getScore() == PartieMonoJoueur.nbDeQuilles && tour != PartieMonoJoueur.nbDeTours) {
+				termine = true;
+			} else if (getScore() == PartieMonoJoueur.nbDeQuilles) {
+				coup += 1;
+			} else {
+				coup++;
+			}
+		} else if (coup == 2) {
+			lesLancers.add(lancer);
+			termine = true;
+			if (tour == PartieMonoJoueur.nbDeTours && !(getScore() < PartieMonoJoueur.nbDeQuilles)) {
+				coup++;
+				termine = false;
+			}
+		} else if (coup == 3) {
+			lesLancers.add(lancer);
+			termine = true;
+		}
+
+		return !termine;
+	}
 }
 
-public Tour (int lance1){
-    this.lance1 = lance1 ;
-    this.lance2 = 0; 
-}
-
-public int getLance1 () {
-    return lance1 ; 
-}
-
-public int getLance2 () {
-    return lance2 ; 
-}
-
-public int nbPoint (Tour tour2){
-if (lance1== 10){
-    return 10 + tour2.getLance1() + tour2.getLance2() ; 
-}
- else if (lance1 + lance2 == 10){
-    return 10 + tour2.getLance1() ; 
- }
-else return lance1 + lance2 ; 
-
-}
-
-public int getCompteur () {
-    return compteur ; 
-}
-
-
-}
-
-
-
-
-
-//nb tombé en lancé 1 et l'autre en 2èm tour
-//nb de quilles lancées du 1er et 2ème tour si pas strike ou spair
-//trike 10+bonus pair et idem pour spair ? 
-
-
-//la variable add est statique donc elle change pour toute les tours
-//genre à notre tour3 elle vaut 3, elle vaut 3 aussi à notre tour1 et à notre tour2
-//le compteur qui lui est unique à chaque tour vaut donc le add (qui change à chaque fois)
-//plus 1.
